c147e42f6a72b3079a99750a35d4bc87942e26f4
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-10-04 20:35:32 -0600

    small fix

--- web/src/php_templates/header_shopping_cart.php
+++ web/src/php_templates/header_shopping_cart.php
@@ -1,2 +1,2 @@
 <?php
-echo '<header><nav><div class="nav-wrapper blue"><a href="https://wilson-cs-313.herokuapp.com/" class="brand-logo"><i class="material-icons right">home</i></a><ul id="nav-mobile" class="right"><li><a href="./week03/view_cart.php"><i class="material-icons">shopping_cart</i></a></li></ul></div></nav></header>';
+echo '<header><nav><div class="nav-wrapper blue"><a href="https://wilson-cs-313.herokuapp.com/" class="brand-logo"><i class="material-icons right">home</i></a><ul id="nav-mobile" class="right"><li><a href="../week03/view_cart.php"><i class="material-icons">shopping_cart</i></a></li></ul></div></nav></header>';
