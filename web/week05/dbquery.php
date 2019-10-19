a9567d50ba259ce7625b3efb6a8b8f7478b3d226
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-10-19 09:44:42 -0600

    check

--- web/week05/dbquery.php
+++ web/week05/dbquery.php
@@ -5,7 +5,7 @@ $db = get_db();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     header("Content-Type: application/json; charset=UTF-8");
     $obj = json_decode($_GET["x"], false);
-    $tableName = $obj->type;
+    $tableName = "processor";
     $statement = $db->prepare(`SELECT * FROM $tableName`);
     $statement->execute();
 
