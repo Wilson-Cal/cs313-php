ca47cd6b829eb59187266852f73acc4b41d7db43
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-10-23 15:25:08 -0600

    Added Week 6 Boilerplate

---
+++ web/week06/dbquery.php
@@ -0,0 +1,19 @@
+<?php
+require "./dbconnect.php";
+$db = get_db();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    header("Content-Type: application/json; charset=UTF-8");
+    $obj = json_decode($_POST["x"], false);
+    $tableName = $obj->type;
+    $query = "SELECT * from " . $tableName;
+    $statement = $db->prepare($query);
+    $statement->execute();
+
+    $dbdata = array();
+    // Go through each result
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $dbdata[] = $row;
+    }
+    echo json_encode($dbdata);
+}
