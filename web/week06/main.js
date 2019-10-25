771d99ea511b614d93bbb6f0e6f2047c820025ca
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-10-25 13:57:09 -0600

    error check

--- web/week06/main.js
+++ web/week06/main.js
@@ -413,7 +413,7 @@ function sortTable(n) {
 }
 
 async function getFavorites() {
-    let requestObj = { user_id: 1 };
+    let requestObj = { type: 'favorite', user_id: 1 };
     favorites = await Get("dbquery.php", `x=${JSON.stringify(requestObj)}`);
 }
 
