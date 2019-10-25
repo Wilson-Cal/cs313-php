d7474d92c36bbb120f7c0a112ba5a1a89c81e8eb
Cal Wilson <calwilson@Cals-MacBook-Pro.local>
2019-10-25 14:15:46 -0600

    error check

--- web/week06/dbquery.php
+++ web/week06/dbquery.php
@@ -21,11 +21,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $table_part_name = $favorite['table_part_name'];
             $part_id = $favorite['part_id'];
             $query = "SELECT * from $table_part_name WHERE part_id = $part_id";
-            $statement = $db->prepare($query);
-            $statement->execute();
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                $dbdata[] = $row;
-            }
+            // $statement = $db->prepare($query);
+            // $statement->execute();
+            // while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            //     $dbdata[] = $row;
+            // }
         }
         echo json_encode($dbdata);
     } else {
